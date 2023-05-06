df43940ee4becd09f0cba38b3a0107169b4d11c5
dewpe000 <dewpe000@naver.com>
2023-05-06 16:54:32 +0900

    implement survey page excluding http request

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Survey} from './UserPage/components/Survey'
 
 function App() {
+
+  let temp = 1
+
   return (
     <div className="App">
+      <Survey surveyId={temp}></Survey>
     </div>
   );
 }
