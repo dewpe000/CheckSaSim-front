e5c5c7b890419888a464884a0554bb63bc63e4e7
LeeJueun <dlwndms2544@hanyang.ac.kr>
2023-05-06 13:09:19 +0900

    feat: implements left side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Survey } from './UserPage/components/Survey';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Survey />
     </div>
   );
 }
--- src/UserPage/components/Survey.tsx
+++ src/UserPage/components/Survey.tsx
@@ -1,6 +1,24 @@
-export function SideBar() {
+import { SideBar } from "./SideBar";
+import { surveyInfoType } from "./SideBar";
+
+export function Survey() {
+    const surveyData: surveyInfoType[] = [
+        {
+          title : "MBTI 검사",
+          week : 1
+        },
+        {
+          title : "MBTI 검사",
+          week : 2
+        },
+        {
+          title : "MBTI 검사",
+          week : 3
+        },
+      ]
     return (
         <div className="App">
+            <SideBar isAdmin={true} surveyInfo={surveyData}/>
         </div>
     );
 }
